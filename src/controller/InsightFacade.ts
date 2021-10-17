22ce0ac2e6fb0038a626489e72d51c2d1455756b
aquan01 <aquan01@students.cs.ubc.ca>
2021-10-16 21:35:50 -0700

    should work now havent tested

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -50,6 +50,8 @@ let checkFormat: (input: string) => boolean = function(input: string) {
 
 	return true;
 };
+
+
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -65,7 +67,6 @@ export default class InsightFacade implements IInsightFacade {
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		let dataSet: EnumDataItem[] = [];
 		let promises: any[] = [];
-
 		if (!checkFormat(id)) {
 			return Promise.reject("Invalid ID");
 		}
@@ -100,10 +101,9 @@ export default class InsightFacade implements IInsightFacade {
 		return Promise.resolve(id);
 	}
 
-	private static intersection(setA: any, setB: any) {
+	private intersection(setA: any, setB: any) {
 		let intersect = new Set();
 		setB.forEach((elem: unknown) => {
-			// console.log(elem + " " + setA.has(elem));
 			if (setA.has(elem)) {
 				intersect.add(elem);
 			}
@@ -112,7 +112,7 @@ export default class InsightFacade implements IInsightFacade {
 		return intersect;
 	}
 
-	private static union(setA: any, setB: any) {
+	private union(setA: any, setB: any) {
 		let retval = new Set(setA);
 		setB.forEach((elem: unknown) => {
 			retval.add(elem);
@@ -120,51 +120,19 @@ export default class InsightFacade implements IInsightFacade {
 		return retval;
 	}
 
-	private static equals(num: number, dataset: any, field: string) {
-		if (!(typeof num === "number")){
-			return Promise.reject("Must use a number for EQ");
-		}
-		let retval = new Set();
-		dataset.forEach((elem: any) => {
-			if (num === elem[field]) {	// THIS IS DEPENDENT ON HOW THE DATASETS ARE STORED
-				retval.add(elem);
-			}
-		});
-		return Promise.resolve([retval, dataset]);
+	private equals(a: number, b: number) {
+		return a === b;
 	}
 
-	private static greaterThan(num: number, dataset: any, field: string) {
-		if (!(typeof num === "number")){
-			return Promise.reject("Must use a number for GT");
-		}
-		dataset = InsightFacade.getDataset(dataset);
-
-		let retval = new Set();
-		dataset.forEach((elem: any) => {
-			if (num < elem[field]) {	// THIS IS DEPENDENT ON HOW THE DATASETS ARE STORED
-				retval.add(elem);
-			}
-		});
-
-		return Promise.resolve([retval, dataset]);
+	private  greaterThan(a: number, b: number) {
+		return a > b;
 	}
 
-	private static lessThan(num: number, dataset: any, field: string) {
-		if (!(typeof num === "number")){
-			return Promise.reject("Must use a number for LT");
-		}
-		dataset = InsightFacade.getDataset(dataset);
-		let retval = new Set();
-		dataset.forEach((elem: any) => {
-			if (num > elem[field]) {	// THIS IS DEPENDENT ON HOW THE DATASETS ARE STORED
-				retval.add(elem);
-			}
-		});
-
-		return Promise.resolve([retval, dataset]);
+	private lessThan(a: number, b: number) {
+		return a < b;
 	}
 
-	private queryComparator(query: any, logic: (num: number, dataset: any, field: string) => (Promise<any[]>)){
+	private queryComparator(query: any, comparator: any){
 		let FIELDS = ["avg" , "pass" , "fail" , "audit" , "year"];
 		let keys = Object.keys(query);
 		if(keys.length !== 1) {
@@ -172,12 +140,27 @@ export default class InsightFacade implements IInsightFacade {
 		}
 		let value = query[keys[0]];
 		let split = keys[0].split("_", 1);
-		let dataset = split[0];
+		let datasetName = split[0];
+
 		let	field: string = split[1];
-		if (typeof value !== "number" || InsightFacade.doesntExist(dataset) || !FIELDS.includes(field)) {
+		if (typeof value !== "number" || !FIELDS.includes(field)) {
 			return Promise.reject("invalid comparator field");
 		}
-		return Promise.resolve([logic(value, dataset, field), dataset]);
+		let dataset = this.getDataset(datasetName);
+		if (dataset) {
+			let retval = new Set();
+			dataset.forEach((elem: any) => {
+				elem.forEach((row: any) => {
+					if (comparator(row[field], value)) {
+						retval.add(elem);
+					}
+				});
+
+			});
+			return Promise.resolve([retval, datasetName]);
+		} else {
+			return Promise.reject("dataset doesnt exist");
+		}
 	}
 
 	private querySComparator(query: any): Promise<any> {
@@ -188,13 +171,26 @@ export default class InsightFacade implements IInsightFacade {
 		}
 		let value = query[keys[0]];
 		let split = keys[0].split("_", 1);
-		let dataset = split[0];
+		let datasetName = split[0];
 		let	field: string = split[1];
 
-		if (typeof value !== "number" || InsightFacade.doesntExist(dataset) || !FIELDS.includes(field)) {
+		if (typeof value !== "string" ||  !FIELDS.includes(field)) {
 			return Promise.reject("invalid string comparator field");
 		}
-		return Promise.resolve([ dataset]);
+		let dataset = this.getDataset(datasetName);
+		let retval = new Set();
+		if (dataset) {
+			dataset[0].data.forEach((elem: any) => {
+				elem.forEach((row: any) => {
+					if(row[field] === value){
+						retval.add(row);
+					}
+				});
+			});
+		} else{
+			return Promise.reject("dataset doesnt exist");
+		}
+		return Promise.resolve([ dataset, datasetName]);
 	}
 
 	private queryLogic(query: any, logic: (a: any, b: any) => any): Promise<any>{
@@ -202,16 +198,14 @@ export default class InsightFacade implements IInsightFacade {
 		if(keys.length !== 2) {
 			return Promise.reject("more than one logic query");
 		}
-		return this.nextQuery(keys[0]).then((set1) => {
-			return this.nextQuery(keys[1]).then((set2) => {
+		return this.nextQuery(query[keys[0]]).then((set1) => {
+			return this.nextQuery(query[keys[1]]).then((set2) => {
 				if (set1[1] !== set2[1]) {	// Checking database names
 					return Promise.reject("More than one dataset referenced");
 				}
 				return Promise.resolve(logic(set1, set2));
 			});
 		});
-
-		return Promise.reject("dasfa Json");
 	}
 
 	private nextQuery(query: any): Promise<any>{
@@ -222,19 +216,19 @@ export default class InsightFacade implements IInsightFacade {
 		let key = keys[0];
 		switch (key) {
 		case "AND": {
-			return this.queryLogic(query[key], InsightFacade.intersection);
+			return this.queryLogic(query[key], this.intersection);
 		}
 		case "OR": {
-			return this.queryLogic(query[key],InsightFacade.union);
+			return this.queryLogic(query[key],this.union);
 		}
 		case "EQ": {
-			return this.queryComparator(query[key], InsightFacade.equals);
+			return this.queryComparator(query[key], this.equals);
 		}
 		case "GT": {
-			return this.queryComparator(query[key], InsightFacade.greaterThan);
+			return this.queryComparator(query[key], this.greaterThan);
 		}
 		case "LT": {
-			return this.queryComparator(query[key], InsightFacade.lessThan);
+			return this.queryComparator(query[key], this.lessThan);
 		}
 		case "NOT": {
 			return this.queryNot(query[key]);
@@ -287,11 +281,11 @@ export default class InsightFacade implements IInsightFacade {
 		return Promise.resolve(list);
 	}
 
-	private static getDataset(dataset: any) {
-		return Promise.reject("Not implemented.");
+	private getDataset(dataset: any) {
+		return this.data.get(dataset);
 	}
-	private static doesntExist(dataset: string) {
-		return undefined;
+	private hasDataset(dataset: string) {
+		return this.data.has(dataset);
 	}
 
 	private queryNot(queryElement: any) {
